0f96d5dd0be98ce3f973592daaac084970281d5a
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-12 17:19:02 +0100

    jsnack3

--- jsnack3/app.js
+++ jsnack3/app.js
@@ -1,8 +1,26 @@
+// CICLO FOR
+
+// let somma = 0;
+
+// for(let i = 0; i < 5; i++) {
+//     const askNumber = parseInt(prompt('Inserisci un numero.'));
+//     somma = somma + askNumber;
+// }
+
+// console.log(somma);
+
+// **************************************************
+
+// CICLO WHILE
+
 let somma = 0;
 
-for(let i = 0; i < 5; i++) {
+let j = 0;
+
+while (j < 5) {
     const askNumber = parseInt(prompt('Inserisci un numero.'));
     somma = somma + askNumber;
+    j++;
 }
 
-console.log(somma)
+console.log(somma);
