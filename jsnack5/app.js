93ea74d970baea437fba2dca17fc4b9fb56c40e9
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-12 19:08:51 +0100

    jsnack5

--- jsnack5/app.js
+++ jsnack5/app.js
@@ -1,1 +1,13 @@
-console.log('Ciao a tutti!');
+const numeriDispari = [];
+
+for(let i = 0; i < 6; i++) {
+    const chiediNumero = parseInt(prompt('Dimmi un numero.'));
+
+    if (chiediNumero % 2 == 0) {
+        console.log('Sotto troverai i numeri dispari!');
+    } else {
+        numeriDispari.push(chiediNumero);
+    }
+}
+
+console.log(numeriDispari);
