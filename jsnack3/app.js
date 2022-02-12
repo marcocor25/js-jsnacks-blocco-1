cced840a03c49b481e041f6c257b9e2bfa1d8bce
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-12 16:56:23 +0100

    jsnack3 con ciclo for completato

--- jsnack3/app.js
+++ jsnack3/app.js
@@ -1,1 +1,8 @@
-console.log('Ciao a tutti!');
+let somma = 0;
+
+for(let i = 0; i < 5; i++) {
+    const askNumber = parseInt(prompt('Inserisci un numero.'));
+    somma = somma + askNumber;
+}
+
+console.log(somma)
