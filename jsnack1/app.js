fb3cc87a56092f34c63b48ce98723aa1cb40b06f
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-12 16:37:02 +0100

    jsnack1

--- jsnack1/app.js
+++ jsnack1/app.js
@@ -1,1 +1,19 @@
-console.log('Ciao a tutti!');
+const numArray = [];
+
+while (numArray.length !== 2) {
+    const number = parseFloat(prompt('Inserisci un numero.'));
+    
+    if (!isNaN(number)) {
+        numArray.push(number);
+    }
+}
+
+const [primo, secondo] = numArray;
+
+let maggiore = primo;
+
+if (primo < secondo) {
+    maggiore = secondo;
+}
+
+console.log(maggiore);
