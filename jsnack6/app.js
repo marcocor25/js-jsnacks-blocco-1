d43bf29c826d08a5462f3647e9397330c753fc63
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-12 19:52:32 +0100

    jsnack6

--- jsnack6/app.js
+++ jsnack6/app.js
@@ -1,1 +1,15 @@
-console.log('Ciao a tutti!');
+const numeri = [];
+
+for(let i = 0; i < 5; i++) {
+    const chiediNumeri = parseInt(prompt('Inserisci dei numeri a tua scelta'));
+    numeri.push(chiediNumeri);
+}
+
+alert(`Hai scelto questi numeri: ${numeri}.
+Tieni a mente uno di questi numeri.`);
+
+const ricordaNumero = parseInt(prompt('Inserisci il numero da te scelto e ti dirò il risultato elevato al cubo! :)'));
+
+const numeroCubo = ricordaNumero ** 3;
+
+alert(`${ricordaNumero} elevato al cubo equivale a: ${numeroCubo}`);
