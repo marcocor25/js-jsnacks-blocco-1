f668a88ed3e7ce065d5c094759c5af6c59d18bf7
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-12 17:57:57 +0100

    jsnack4

--- jsnack4/app.js
+++ jsnack4/app.js
@@ -1,1 +1,34 @@
-console.log('Ciao a tutti!');
+// CREO UNA VARIABILE PER CHIEDERE IL NOME
+const chiediNome = prompt('Come ti chiami?');
+
+// CREO VARIABILE PER SAPERE CHI È AUTORIZZATO
+let autorizzato = 0;
+
+// CREO ARRAY DI INVITATI
+const invitati = [
+    'Lucia',
+    'Giuseppe',
+    'Luigi',
+    'Gianmarco',
+    'Andrea',
+    'Antonio',
+    'Monica',
+    'Jonathan',
+    'Laura',
+    'Vanessa',
+    'Carlo',
+    'Paolo',
+];
+
+for (let i = 0; i < invitati.length; i++) {
+    if (chiediNome == invitati[i]) {
+        autorizzato = 1;
+    }
+}
+
+if(autorizzato){
+    alert('Perfetto, puoi venire alla festa!');
+}
+else{
+    alert('Mi dispiace, non sei stato invitato alla festa...');
+}
