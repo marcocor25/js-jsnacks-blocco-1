2eb60ad1f90e1c023257bd696d3ea5433f26c761
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-13 18:47:09 +0100

    jsnack2

--- jsnack2/app.js
+++ jsnack2/app.js
@@ -1,1 +1,9 @@
-console.log('Ciao a tutti!');
+const chiediParola = prompt('Dimmi una parola.');
+
+const chiediPaorolaDue = prompt("Dimmi un'altra parola.");
+
+if (chiediParola.length > chiediPaorolaDue.length) {
+    console.log(chiediParola, chiediPaorolaDue);
+} else {
+    console.log(chiediPaorolaDue, chiediParola);
+}
