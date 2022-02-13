ae9f6b54eb93c0d34ce7961b5b57b67f00634428
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-13 20:44:39 +0100

    jsnack9

--- jsnack9/app.js
+++ jsnack9/app.js
@@ -1,1 +1,20 @@
-console.log('Ciao a tutti!');
+let array = [];
+
+for(let i = 1; i <= 10; i++) {
+  const numeri = i;
+  array.push(numeri);
+}
+
+console.log(array);
+
+let somma = 0;
+
+for(let j = 0; j < array.length; j++) {
+  somma += array[j];
+}
+
+console.log(somma);
+
+const media = somma / 10;
+
+console.log(media);
