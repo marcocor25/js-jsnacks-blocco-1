2980f1f6c547e8dffdb266bc802b6bcd055cf2e3
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-13 19:44:02 +0100

    jsnack8

--- jsnack8/app.js
+++ jsnack8/app.js
@@ -1,1 +1,19 @@
-console.log('Ciao a tutti!');
+let chiediNumero;
+
+do {
+    chiediNumero = prompt('Dimmi un numero di 4 cifre e io ti darò la somma dei numeri che contiene.');
+    if (parseInt(chiediNumero) < 1000 || parseInt(chiediNumero) > 9999) {
+        alert('Inserisci un numero di 4 cifre, idiota!')
+    }
+} while (parseInt(chiediNumero) < 1000 || parseInt(chiediNumero) > 9999);
+
+let splitNumeri = chiediNumero.split('').map(Number);
+console.log(splitNumeri);
+
+let somma = 0;
+
+for(let i = 0; i < splitNumeri.length; i++) {
+    somma += splitNumeri[i];
+}
+
+console.log(somma);
